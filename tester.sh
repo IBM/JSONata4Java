8ae3e6ce1028172516e2a3f261a1f7806f356775
Nathaniel Mills <wnm3@us.ibm.com>
2019-08-06 17:03:11 -0400

    IBM/JSONata4Java#20

--- tester.sh
+++ tester.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -cp target/JSONata4Java-1.0.9-jar-with-dependencies.jar com.api.jsonata4java.Tester
+java -cp target/JSONata4Java-1.1.0-jar-with-dependencies.jar com.api.jsonata4java.Tester
