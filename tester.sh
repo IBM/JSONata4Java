235287b6b2cb1a31e880eff5b7cd6ca9fa271b7f
wnmills3 <wnmills3@gmail.com>
2022-03-14 13:59:12 -0400

    IBM/JSONata4Java#187

--- tester.sh
+++ tester.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -cp target/JSONata4Java-1.7.2-jar-with-dependencies.jar com.api.jsonata4java.Tester $1
+java -cp target/JSONata4Java-1.7.3-jar-with-dependencies.jar com.api.jsonata4java.Tester $1
