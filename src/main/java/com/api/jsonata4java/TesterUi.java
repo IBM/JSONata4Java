c503a6fa78915828dadf5d0993cb7a7befd4ba84
Martin Bluemel <martin.bluemel@web.de>
2022-09-30 19:55:14 +0200

    Thanks to Jackson's formidable XmlMapper we can also easily processs XML
    Signed-off-by: Martin Bluemel <martin.bluemel@web.de>

--- src/main/java/com/api/jsonata4java/TesterUi.java
+++ src/main/java/com/api/jsonata4java/TesterUi.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -29,11 +30,16 @@ import com.api.jsonata4java.expressions.ParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 
 public class TesterUi {
 
 	private Expressions expressions;
-	private final ObjectMapper mapper = new ObjectMapper();
+	private final ObjectMapper jsonMapper = new ObjectMapper();
+	private final XmlMapper xmlMapper = new XmlMapper();
 
 	private final JTextArea inputArea = new JTextArea();
 	private final JTextArea jsonataArea = new JTextArea();
@@ -49,13 +55,12 @@ public class TesterUi {
 	private final UndoManager undoManJsonata = new UndoManager();
 
 	private static final String REDO_KEY_NAME = "redo";
-    private static final String UNDO_KEY_NAME = "undo";
+	private static final String UNDO_KEY_NAME = "undo";
 
 	private final KeyStroke undoKey = KeyStroke.getKeyStroke("control Z");
 	private final KeyStroke redoKey = KeyStroke.getKeyStroke("control Y");
 
-
-	private TesterUi() throws IOException {
+	protected TesterUi() throws IOException {
 
 		inputArea.setText(readFile("src/test/resources/exerciser/address.json"));
 		jsonataArea.setText(readFile("src/test/resources/exerciser/addressExpression.jsonata"));
@@ -68,7 +73,8 @@ public class TesterUi {
 
 		frame.setTitle("JSONata4Java Tester UI");
 		final Dimension screendim = Toolkit.getDefaultToolkit().getScreenSize();
-		// frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+		// frame.setLocation(dim.width/2-frame.getSize().width/2,
+		// dim.height/2-frame.getSize().height/2);
 		frame.getContentPane().add(splitPane);
 		this.frame.addWindowListener(new WindowAdapter() {
 			@Override
@@ -92,65 +98,71 @@ public class TesterUi {
 
 	private void listenToInputChanges() {
 		inputArea.getDocument().addDocumentListener(new DocumentListener() {
-	        @Override
-	        public void removeUpdate(DocumentEvent e) {
-	        	inputChanged();
-	        }
-	        @Override
-	        public void insertUpdate(DocumentEvent e) {
-	        	inputChanged();
-	        }
-	        @Override
-	        public void changedUpdate(DocumentEvent arg0) {
-	        	inputChanged();
-	        }
-	    });
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				inputChanged();
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				inputChanged();
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent arg0) {
+				inputChanged();
+			}
+		});
 	}
 
 	private void listenToJsonataChanges() {
 		jsonataArea.getDocument().addDocumentListener(new DocumentListener() {
-	        @Override
-	        public void removeUpdate(DocumentEvent e) {
-	        	jsonataChanged();
-	        }
-	        @Override
-	        public void insertUpdate(DocumentEvent e) {
-	        	jsonataChanged();
-	        }
-	        @Override
-	        public void changedUpdate(DocumentEvent arg0) {
-	        	jsonataChanged();
-	        }
-	    });
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				jsonataChanged();
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				jsonataChanged();
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent arg0) {
+				jsonataChanged();
+			}
+		});
 	}
 
 	private void makeUndoable(JTextArea area, UndoManager undoManager) {
 		area.getDocument().addUndoableEditListener(event -> undoManager.addEdit(event.getEdit()));
 		area.getActionMap().put(UNDO_KEY_NAME, new AbstractAction(UNDO_KEY_NAME) {
 			private static final long serialVersionUID = 1L;
+
 			@Override
-            public void actionPerformed(ActionEvent evt) {
-                try {
-                    if (undoManager.canUndo()) {
-                    	undoManager.undo();
-                    }
-                } catch (CannotUndoException ignore) {
-                }
-            }
-        });
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					if (undoManager.canUndo()) {
+						undoManager.undo();
+					}
+				} catch (CannotUndoException ignore) {
+				}
+			}
+		});
 		area.getInputMap().put(undoKey, UNDO_KEY_NAME);
 		area.getActionMap().put(REDO_KEY_NAME, new AbstractAction(REDO_KEY_NAME) {
 			private static final long serialVersionUID = 1L;
-				@Override
-	            public void actionPerformed(ActionEvent evt) {
-	                try {
-	                    if (undoManager.canRedo()) {
-	                    	undoManager.redo();
-	                    }
-	                } catch (CannotRedoException ignore) {
-	                }
-	            }
-	        });
+
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					if (undoManager.canRedo()) {
+						undoManager.redo();
+					}
+				} catch (CannotRedoException ignore) {
+				}
+			}
+		});
 		area.getInputMap().put(redoKey, REDO_KEY_NAME);
 	}
 
@@ -188,7 +200,14 @@ public class TesterUi {
 	private void map() {
 		JsonNode inNode = null;
 		try {
-			inNode = mapper.readTree(inputArea.getText());
+			switch (getFormat(inputArea.getText())) {
+			case XML:
+				inNode = xmlMapper.readTree(inputArea.getText());				
+				break;
+			default:
+				inNode = jsonMapper.readTree(inputArea.getText());
+				break;
+			}
 		} catch (JsonProcessingException | RuntimeException e) {
 			System.err.println("Input error: " + e.getMessage());
 			this.outputArea.setText("Input error: " + e.getMessage());
@@ -198,7 +217,7 @@ public class TesterUi {
 		}
 		try {
 			final JsonNode outNode = this.expressions.evaluate(inNode);
-			final String newOutput = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(outNode);
+			final String newOutput = jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(outNode);
 			this.outputArea.setText(newOutput);
 			System.out.println("Mapped successfully");
 			this.inputArea.setBackground(new Color(0xEEFFFF));
@@ -212,10 +231,35 @@ public class TesterUi {
 		}
 	}
 
-	private static String readFile(String filePath) throws IOException {
+	enum Format { XML, JSON };
+
+	private Format getFormat(String text) {
+		if (text.trim().startsWith("<")) {
+			return Format.XML;
+		}
+		return Format.JSON;
+	}
+
+	protected static String readFile(String filePath) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(filePath));
 		String s = new String(bytes, StandardCharsets.UTF_8);
 		return s;
+	}
+
+	protected String jsonToXml(final String json) throws IOException {
+		JsonNode node = jsonMapper.readValue(json, JsonNode.class);
+		xmlMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+		xmlMapper.configure(ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true);
+		xmlMapper.configure(ToXmlGenerator.Feature.WRITE_XML_1_1, true);
+		ObjectWriter ow = xmlMapper.writer().withRootName("root");
+		StringWriter w = new StringWriter();
+		ow.writeValue(w, node);
+		return w.toString();
+	}
+
+	protected String xmlToJson(final String xml) throws IOException {
+		JsonNode node = xmlMapper.readTree(xml.getBytes());
+		return  jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(node);
 	}
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/api/jsonata4java/TesterUiTest.java
@@ -0,0 +1,59 @@
+package com.api.jsonata4java;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+
+public class TesterUiTest {
+
+	TesterUi testerUi;
+
+	@Before
+	public void setUp() throws IOException {
+		testerUi = new TesterUi();
+	}
+
+	@Test
+	public void testJsonToXml() throws IOException {
+		System.out.println(testerUi.jsonToXml(TesterUi.readFile("src/test/resources/exerciser/address.json")));
+	}
+
+	@Test
+	public void testXmlToJson() throws IOException {
+		assertEquals(minifyJson(TesterUi.readFile("src/test/resources/exerciser/address.json")),
+				minifyJson(testerUi.xmlToJson(TesterUi.readFile("src/test/resources/exerciser/address.xml"))));
+	}
+
+	public static String minifyJson(final String in) {
+		final StringWriter sw = new StringWriter();
+		final JsonFactory factory = new JsonFactory();
+		try (final JsonGenerator gen = factory.createGenerator(sw)) {
+			final JsonParser parser = factory.createParser(in);
+			while (parser.nextToken() != null) {
+				gen.copyCurrentEvent(parser);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return sw.getBuffer().toString();
+	}
+
+//	public static String minifyXml(final String in) {
+//		return XalanWrapper.xslt(in, /* style sheet */
+//				"<?xml version=\"1.0\"?>"
+//						+ "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">"
+//						+ "<xsl:output indent=\"no\"/>" + "<xsl:strip-space elements=\"*\"/>"
+//						+ "<xsl:template match=\"@*|node()\">"
+//						+ "<xsl:copy><xsl:apply-templates select=\"@*|node()\"/></xsl:copy>" + "</xsl:template>"
+//						+ "</xsl:stylesheet>",
+//				/* no params */ null, "xml", "UTF-8", false, null, !in.strip().startsWith("<?xml"));
+//	}
+}
