11b56380deb228ba8a022cfd34900aeddf60da1d
wnmills3 <wnmills3@gmail.com>
2022-11-17 11:19:21 -0500

    IBM/JSONata4Java#232
    Signed-off-by: Martin Bluemel <martin.bluemel1@vodafone.com>

--- testerui.cmd
+++ testerui.cmd
@@ -1,1 +1,1 @@
-java -cp target/JSONata4Java-2.2.2-jar-with-dependencies.jar com.api.jsonata4java.testerui.TesterUI
+java -cp target/JSONata4Java-2.2.3-jar-with-dependencies.jar com.api.jsonata4java.testerui.TesterUI
