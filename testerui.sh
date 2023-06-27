3990d135aa3d7ed4056cf61220f52abdde2b7986
wnm3 <wnm3@us.ibm.com>
2023-06-27 09:48:48 -0400

    IBM/JSONata4Java#243

--- testerui.sh
+++ testerui.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -cp target/JSONata4Java-2.4.2-jar-with-dependencies.jar com.api.jsonata4java.testerui.TesterUI
+java -cp target/JSONata4Java-2.4.3-jar-with-dependencies.jar com.api.jsonata4java.testerui.TesterUI
