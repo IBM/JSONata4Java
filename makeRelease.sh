7ce19cff5ce58fff6086f1c82c387aa012db54a2
wnm3 <wnm3@us.ibm.com>
2024-01-25 09:05:54 -0500

    IBM/JSONata4Java#294

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
+export OLD_JAVA_HOME=$JAVA_HOME
+export OLD_PATH=$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.17+8/Contents/Home
+export PATH=$JAVA_HOME:$PATH
 mvn clean install deploy -Prelease
+export JAVA_HOME=$OLD_JAVA_HOME
+export PATH=$OLD_PATH
+
