4696da17e372d603e090dd170f2dbf76cd6724d6
wnm3 <wnm3@us.ibm.com>
2025-05-08 11:08:03 -0400

    IBM/JSONata4Java#351

--- tester.sh
+++ tester.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -cp target/JSONata4Java-2.5.1-jar-with-dependencies.jar com.api.jsonata4java.Tester $1
+java -cp target/JSONata4Java-2.5.2-jar-with-dependencies.jar com.api.jsonata4java.Tester $1
